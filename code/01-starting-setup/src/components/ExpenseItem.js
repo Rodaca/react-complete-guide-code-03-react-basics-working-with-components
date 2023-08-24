59649aab42eae4de392853625ef7f886662e9831
Rodacay <rodaca97@hotmail.com>
2023-08-24 09:36:46 -0500

    first commit

---
+++ code/01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+ function ExpenseItem(props) {
+     /* const expenseDate=title
+     const expenseTitle=amount
+     const expenseAmount=date */
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </Card>
+    );
+    
+ } 
+ export default ExpenseItem; 
